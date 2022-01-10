5f74b77acfc325801a039f96cc45bfe074d3d8e6
Preethi <preethi.patrick5@gmail.com>
2022-01-10 01:51:00 +0000

    fixed gitignore

--- plot.py
+++ plot.py
@@ -92,6 +92,9 @@ for for_data in datasets:
                         data['dataset'] == for_data) & (data['experiment'] == experiment)].iterrows():
                     fpr = row['fpr']
                     tpr = row['tpr']
+                    if for_model in ["resnet_lstm"]:
+                        fpr = gaussian_filter1d(fpr, sigma=10)
+                        tpr = gaussian_filter1d(tpr, sigma=10)
                     score = row['score']
                     plt.plot(fpr, tpr, label=f"{display_name}-{score}")
                     plt.xlabel("True Positive Rate")
@@ -99,32 +102,5 @@ for for_data in datasets:
                     plt.legend(loc=4)
             plt.savefig(f"graphs/roc_{for_data}_{for_training}_{experiment_name}.png")
     plt.close()
-
-# for for_data in datasets:
-#     for for_training in trainings:
-#         for for_model in models:
-#             plt.figure(figsize=(16, 12), dpi=80)
-#             for index, row in data[(data['model'] == for_model) & (data['data_state'] == for_training) & (
-#                     data['dataset'] == for_data)].iterrows():
-#                 if for_model in ["resnet", "xception"]:
-#                     display_name = f"CNN_{for_model}"
-#                 elif for_model == "resnet_lstm":
-#                     display_name = "CNN+LSTM"
-#                 elif for_model == "resnet3":
-#                     display_name = "3D-CNN"
-#                 experiment_name = "mixed_augmentations" if row['experiment'] == "random_augmentation" else row['experiment']
-#                 fpr = row['fpr']
-#                 tpr = row['tpr']
-#                 score = row['score']
-#                 if for_model in ["resnet_lstm"]:
-#                     fpr = gaussian_filter1d(fpr, sigma=10)
-#                     tpr = gaussian_filter1d(tpr, sigma=10)
-#                 plt.plot(fpr, tpr, label=f"{experiment_name}-{score}")
-#                 plt.legend(loc=4)
-#             plt.savefig(f"graphs/roc_{for_data}_{for_training}_{for_model}.png")
-#             plt.close()
-#         break
-#     break
-# break
 plt.legend(loc=4)
 # plt.show()
